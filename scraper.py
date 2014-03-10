0c9b6b3af52bfc1aa3a123b3a9a68cb4535ec605
Jack Markley <jack.markley@gmail.com>
2014-03-10 07:01:08 +0100

    REST test

--- scraper.py
+++ scraper.py
@@ -1,7 +1,27 @@
 import requests
 import pdb
 import sys
+from pprint import pprint
 from bs4 import BeautifulSoup
+import json
+
+def add_address(listing):
+    api_url = 'http://maps.googleapis.com/maps/api/geocode/json'
+    loc = listing['location']
+    latlng_tmpl = "{data-latitude},{data-longitude}"
+    parameters = {
+        'sensor': 'false',
+        'latlng': latlng_tmpl.format(**loc),
+    }
+    resp = requests.get(api_url, params=parameters)
+    resp.raise_for_status() # <- this is a no-op if all is well
+    data = json.loads(resp.text)
+    if data['status'] == 'OK':
+        best = data['results'][0]
+        listing['address'] = best['formatted_address']
+    else:
+        listing['address'] = 'unavailable'
+    return listing
 
 def fetch_search_results(
     query="", minAsk=None, maxAsk=None, bedrooms=None
@@ -28,7 +48,7 @@ def parse_source(html, encoding='utf-8'):
 def extract_listings(parsed):
     location_attrs = {'data-latitude': True, 'data-longitude': True}
     listings = parsed.find_all('p', class_='row', attrs=location_attrs)
-    extracted = []
+    #extracted = [] we don't like making huge lists if we don't have to
     for listing in listings:
         location = {key: listing.attrs.get(key, '') for key in location_attrs}
         link = listing.find('span', class_='pl').find('a')
@@ -40,19 +60,18 @@ def extract_listings(parsed):
             'price': price_span.string.strip(),             # and me
             'size': price_span.next_sibling.strip(' \n-/')  # me too
         }
-        extracted.append(this_listing)
-    return extracted
+        #extracted.append(this_listing) no list == no appending to it
+        yield this_listing  # This is the only change you need to make
+    #return extracted we're yielding today, not returning.
 
 if __name__ == '__main__':
-    import pprint
     if len(sys.argv) > 1 and sys.argv[1] == 'test':
         html, encoding = read_search_results()
     else:
         html, encoding = fetch_search_results(
             minAsk=500, maxAsk=1000, bedrooms=2
         )
-    doc = parse_source(html, encoding)
-    listings = extract_listings(doc)
-    for indListing in listings:
-        pprint.pprint(indListing)
-        print("")
+    doc = parse_source(html, encoding)    # above here is the same
+    for listing in extract_listings(doc): # change everything below
+        listing = add_address(listing)
+        pprint(listing)
